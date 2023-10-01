508f02cdf520d08bc5c22c9504412a7138deab12
alvar <alvaro.alfaya@sga.puminas.br>
2023-10-01 18:15:07 -0300

    Código Fonte do Projeto, Funcionalidades Registro e Login

---
+++ codigo-fonte/BrechoLaFripAtelier/Areas/Identity/Data/BrechoLaFripAtelierContext.cs
@@ -0,0 +1,22 @@
+﻿using BrechoLaFripAtelier.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrechoLaFripAtelier.Data;
+
+public class BrechoLaFripAtelierContext : IdentityDbContext<UsuarioModel>
+{
+    public BrechoLaFripAtelierContext(DbContextOptions<BrechoLaFripAtelierContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
