bd4412e1041ae80ba3a5d8ff4c37fb5ceb4a1108
Álvaro <91163177+alvaroalfaya@users.noreply.github.com>
2023-10-25 19:54:46 -0300

    Adicionado Crud de Produtos

---
+++ codigo-fonte/BrechoLaFripAtelier/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BrechoLaFripAtelier.Models;
+
+namespace BrechoLaFripAtelier.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BrechoLaFripAtelier.Models.MyDbContext _context;
+
+        public DetailsModel(BrechoLaFripAtelier.Models.MyDbContext context)
+        {
+            _context = context;
+        }
+
+      public Product Product { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
---
+++ codigo-fonte/BrechoLaFripAtelier/Pages/Products/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BrechoLaFripAtelier.Models;
+
+namespace BrechoLaFripAtelier.Pages.Products
+{
+    public class EditModel : PageModel
+    {
+        private readonly BrechoLaFripAtelier.Models.MyDbContext _context;
+
+        public EditModel(BrechoLaFripAtelier.Models.MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product =  await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            Product = product;
+           ViewData["PartnerId"] = new SelectList(_context.Partners, "Id", "CPFnumber");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(Product.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
