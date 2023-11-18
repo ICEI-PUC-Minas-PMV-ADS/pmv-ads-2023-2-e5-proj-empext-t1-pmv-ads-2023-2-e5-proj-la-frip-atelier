e942c857c5632f9d0ac9c19308364d1d767c20f6
Talles Góis <tallesmonteiro0@gmail.com>
2023-11-18 00:23:13 -0300

    Implementa funcionalidade Relatório de Vendas Por Periodo

--- codigo-fonte/BrechoLaFripAtelier/Pages/Sales/Result.cshtml.cs
+++ codigo-fonte/BrechoLaFripAtelier/Pages/Sales/Result.cshtml.cs
@@ -15,15 +15,20 @@ namespace BrechoLaFripAtelier.Pages.Sales
 
         public IList<Sale> Sale { get; set; } = default!;
 
-        public async Task OnGetAsync(int? id)
+        public async Task OnGetAsync(int? partnerId,string? date)
         {
             IQueryable<Sale> sales = _context.Sales;
 
-            if (id != null)
+            if (partnerId != null)
             {
-                sales = sales.Where(s => s.Product.Partner.Id == id);
+                sales = sales.Where(s => s.Product.Partner.Id == partnerId);
             }
 
+            if (date != null)
+            {
+                var parsedDate = DateTime.Parse(date);
+                sales = sales.Where(s => s.DateOfSale.Equals(parsedDate));
+            }
 
             Sale = await sales.Include(s => s.Product)
                               .ThenInclude(p => p.Partner)
---
+++ codigo-fonte/BrechoLaFripAtelier/Pages/Sales/Saleday.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using BrechoLaFripAtelier.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrechoLaFripAtelier.Pages.Sales
+{
+    public class SaledayModel : PageModel
+    {
+        private readonly MyDbContext _context;
+
+        public SaledayModel(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<SalesByDayViewModel> SalesByDay { get; set; } = new List<SalesByDayViewModel>();
+
+        public async Task OnGetAsync(DateTime? startDate, DateTime? endDate)
+        {
+            var query = _context.Sales
+                .Include(s => s.Product)
+                .AsQueryable();  // Inicializa a consulta
+
+            if (startDate.HasValue)
+            {
+                query = query.Where(s => s.DateOfSale.Date >= startDate.Value.Date);
+            }
+
+            if (endDate.HasValue)
+            {
+                query = query.Where(s => s.DateOfSale.Date <= endDate.Value.Date);
+            }
+
+            var salesByDay = await query
+                .GroupBy(s => s.DateOfSale.Date)
+                .Select(group => new SalesByDayViewModel
+                {
+                    Date = group.Key,
+                    TotalItemsSold = group.Count(),
+                    TotalAmount = group.Sum(s => s.Product.Price)
+                })
+                .OrderByDescending(s => s.Date)
+                .ToListAsync();
+
+            SalesByDay = salesByDay;
+        }
+    }
+
+    public class SalesByDayViewModel
+    {
+        public DateTime Date { get; set; }
+        public int TotalItemsSold { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
