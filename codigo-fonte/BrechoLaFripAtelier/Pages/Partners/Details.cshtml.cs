a707ffdde11ce941e493504027df1fb34df0fe84
Mychel Costa <mychel@mailbox.org>
2023-11-19 23:06:32 -0300

    Edita páginas de detalhes de Produto e Parceira

---
+++ codigo-fonte/BrechoLaFripAtelier/Pages/Partners/Details.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using BrechoLaFripAtelier.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BrechoLaFripAtelier.Pages.Partners
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MyDbContext _context;
+
+        public DetailsModel(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public Partner Partner { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Partners == null)
+            {
+                return NotFound();
+            }
+
+            var partner = await _context.Partners.FirstOrDefaultAsync(m => m.Id == id);
+            if (partner == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Partner = partner;
+            }
+            return Page();
+        }
+    }
+}
