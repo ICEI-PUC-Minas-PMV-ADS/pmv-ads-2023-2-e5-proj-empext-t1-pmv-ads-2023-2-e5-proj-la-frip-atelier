c36c110d1d5ae6d22f18d9dbbc4a2f7fbd6267d4
Mychel Costa <mychel@mailbox.org>
2023-11-30 18:32:33 -0300

    Cria botão de voltar ao topo
    - Cria botão de voltar ao topo
    - Pergunta de segurança em negrito

--- codigo-fonte/BrechoLaFripAtelier/wwwroot/js/site.js
+++ codigo-fonte/BrechoLaFripAtelier/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const botao = document.querySelector('.btn-top');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 300) {
+        botao.classList.add('show');
+    } else {
+        botao.classList.remove('show');
+    }
+});
