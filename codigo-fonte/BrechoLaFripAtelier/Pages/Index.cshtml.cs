724db43575788ebb5e34b90dd9a07db2b551efc1
Talles Góis <tallesmonteiro0@gmail.com>
2023-12-02 13:55:41 -0300

    Adiciona Dashboard

--- codigo-fonte/BrechoLaFripAtelier/Pages/Index.cshtml.cs
+++ codigo-fonte/BrechoLaFripAtelier/Pages/Index.cshtml.cs
@@ -1,19 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BrechoLaFripAtelier.Models;
+using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
 
-namespace BrechoLaFripAtelier.Pages;
-
-public class IndexModel : PageModel
+namespace BrechoLaFripAtelier.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<IndexModel> _logger;
+        private readonly MyDbContext _dbContext;
 
-    public void OnGet()
-    {
+        public IndexModel(ILogger<IndexModel> logger, MyDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
 
+        public void OnGet()
+        {
+            ViewData["TotalItemsCadastrados"] = _dbContext.Products.Count();
+
+            DateTime hoje = DateTime.Today;
+            ViewData["TotalItemsVendidosNoDia"] = _dbContext.Sales
+                .Count(s => s.DateOfSale == hoje);
+
+            ViewData["ParceirasAguardandoPagamento"] = _dbContext.Sales
+                .Count(s => s.Status == Models.CommissionStatus.Pendente);
+        }
     }
 }
